942c31b814d9c5492b0eba91e0722bdf0325ed9b
alexander.baglay <apofig@gmil.com>
2015-04-03 02:26:07 +0300

    Большой рефакторинг

---
+++ src/main/java/com/codenjoy/dojo/battlecity/model/AITank.java
@@ -0,0 +1,58 @@
+package com.codenjoy.dojo.battlecity.model;
+
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.services.RandomDice;
+
+/**
+ * User: sanja
+ * Date: 14.06.13
+ * Time: 2:51
+ */
+public class AITank extends Tank {
+
+    private final static Dice dice = new RandomDice();
+    private int act;
+
+    public AITank(int x, int y, Direction direction) {
+        super(x, y, direction, dice, 1);
+    }
+
+    @Override
+    public void move() {
+        if (act++ % 10 == 0) {
+            act();
+        }
+
+        int c = 0;
+        int x = 0;
+        int y = 0;
+        do {
+            x = direction.changeX(getX());
+            y = direction.changeY(getY());
+
+            if (field.isBarrier(x, y)) {
+                direction = Direction.random();
+            }
+        } while (field.isBarrier(x, y) && c++ < 10);
+
+        moving = true;
+
+        super.move();
+    }
+
+    @Override
+    public Elements state(Player player, Object... alsoAtPoint) {
+        if (isAlive()) {
+            switch (direction) {
+                case LEFT:  return Elements.AI_TANK_LEFT;
+                case RIGHT: return Elements.AI_TANK_RIGHT;
+                case UP:    return Elements.AI_TANK_UP;
+                case DOWN:  return Elements.AI_TANK_DOWN;
+                default: throw new RuntimeException("Неправильное состояние танка!");
+            }
+        } else {
+            return Elements.BANG;
+        }
+    }
+}
---
+++ src/main/java/com/codenjoy/dojo/battlecity/model/Tank.java
@@ -0,0 +1,138 @@
+package com.codenjoy.dojo.battlecity.model;
+
+import com.codenjoy.dojo.services.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Tank extends MovingObject implements Joystick, Tickable, State<Elements, Player> {
+
+    private Dice dice;
+    private List<Bullet> bullets;
+    protected Field field;
+    private boolean alive;
+    private Gun gun;
+
+    public Tank(int x, int y, Direction direction, Dice dice, int ticksPerBullets) {
+        super(x, y, direction);
+        gun = new Gun(ticksPerBullets);
+        bullets = new LinkedList<Bullet>();
+        speed = 1;
+        moving = false;
+        alive = true;
+        this.dice = dice;
+    }
+
+    void turn(Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public void up() {
+        direction = Direction.UP;
+        moving = true;
+    }
+
+    @Override
+    public void down() {
+        direction = Direction.DOWN;
+        moving = true;
+    }
+
+    @Override
+    public void right() {
+        direction = Direction.RIGHT;
+        moving = true;
+    }
+
+    @Override
+    public void left() {
+        direction = Direction.LEFT;
+        moving = true;
+    }
+
+    @Override
+    public void moving(int newX, int newY) {
+        if (field.isBarrier(newX, newY)) {
+            // do nothing
+        } else {
+            x = newX;
+            y = newY;
+        }
+        moving = false;
+    }
+
+    @Override
+    public void act(int... p) {
+        if (gun.tryToFire()) {
+            Bullet bullet = new Bullet(field, direction, copy(), this, new OnDestroy() {
+                @Override
+                public void destroy(Object bullet) {
+                    Tank.this.bullets.remove(bullet);
+                }
+            });
+            if (!bullets.contains(bullet)) {
+                bullets.add(bullet);
+            }
+        }
+    }
+
+    public Iterable<Bullet> getBullets() {
+        return new LinkedList<Bullet>(bullets);
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+        int xx = x;
+        int yy = y;
+        while (field.isBarrier(xx, yy)) {
+            xx = dice.next(field.size());
+            yy = dice.next(field.size());
+        }
+        x = xx;
+        y = yy;
+        alive = true;
+    }
+
+    public void kill(Bullet bullet) {
+        alive = false;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void removeBullets() {
+        bullets.clear();
+    }
+
+    @Override
+    public void tick() {
+        gun.tick();
+    }
+
+    @Override
+    public Elements state(Player player, Object... alsoAtPoint) {
+        if (isAlive()) {
+            if (player.getTank() == this) {
+                switch (direction) {
+                    case LEFT:  return Elements.TANK_LEFT;
+                    case RIGHT: return Elements.TANK_RIGHT;
+                    case UP:    return Elements.TANK_UP;
+                    case DOWN:  return Elements.TANK_DOWN;
+                    default:    throw new RuntimeException("Неправильное состояние танка!");
+                }
+            } else {
+                switch (direction) {
+                    case LEFT:  return Elements.OTHER_TANK_LEFT;
+                    case RIGHT: return Elements.OTHER_TANK_RIGHT;
+                    case UP:    return Elements.OTHER_TANK_UP;
+                    case DOWN:  return Elements.OTHER_TANK_DOWN;
+                    default:    throw new RuntimeException("Неправильное состояние танка!");
+                }
+            }
+        } else {
+            return Elements.BANG;
+        }
+    }
+}

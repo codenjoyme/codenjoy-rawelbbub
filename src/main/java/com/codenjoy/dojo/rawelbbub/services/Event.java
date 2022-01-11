f707fc4e6394b93daf9ffdf7abdb7efc35a82071
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-12 00:13:18 +0200

    [engine][namdreab][rawelbbub][clifford][verland] Refactoring. Remove code duplicate near Event equals/hashCode.

--- src/main/java/com/codenjoy/dojo/rawelbbub/services/Event.java
+++ src/main/java/com/codenjoy/dojo/rawelbbub/services/Event.java
@@ -69,16 +69,12 @@ public class Event implements EventObject<Event.Type, Integer> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event events = (Event) o;
-        return type == events.type &&
-                value == events.value;
+        return _equals(o);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type);
+        return _hashCode();
     }
 
     @Override

eb7dd8f9642776da61e2c88dc07e74a1e4838cff
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-12 13:44:31 +0200

    [mollymage][clifford][rawelbbub][namdreab] Refactoring near performance test - reorder parameters (expectedCreation, expectedTick, expectedPrint). Added performance test for rawelbbub game.

---
+++ src/test/java/com/codenjoy/dojo/rawelbbub/PerformanceTest.java
@@ -0,0 +1,62 @@
+package com.codenjoy.dojo.rawelbbub;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2012 - 2022 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.codenjoy.dojo.rawelbbub.services.GameRunner;
+import com.codenjoy.dojo.rawelbbub.services.GameSettings;
+import org.junit.Test;
+
+import static com.codenjoy.dojo.rawelbbub.services.GameSettings.Keys.COUNT_AIS;
+import static com.codenjoy.dojo.services.round.RoundSettings.Keys.ROUNDS_ENABLED;
+import static com.codenjoy.dojo.utils.TestUtils.assertPerformance;
+
+public class PerformanceTest {
+
+    @Test
+    public void test() {
+
+        // about 7.5 sec
+        int ais = 20;
+        int players = 50;
+        int ticks = 1000;
+
+        int expectedCreation = 1000;
+        int expectedTick = 3000;
+        int expectedPrint = 3000;
+
+        GameRunner runner = new GameRunner(){
+            @Override
+            public GameSettings getSettings() {
+                return new GameSettings()
+                    .bool(ROUNDS_ENABLED, false)
+                    .integer(COUNT_AIS, ais);
+            }
+        };
+
+        boolean printBoard = false;
+        assertPerformance(runner,
+                players, ticks,
+                expectedCreation, expectedTick, expectedPrint,
+                printBoard);
+    }
+}
